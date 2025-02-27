f84c6ec25fc86f3ad6621c22ae3a64e8cdfd888c
Jason Fleetwood-Boldt <jason@heliosdev.shop>
2025-02-27 11:19:06 -0500

    Adds config.infer_spec_type_from_file_location '

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,6 +62,8 @@ RSpec.configure do |config|
     Rails.root.join('spec/fixtures')
   ]
 
+  config.infer_spec_type_from_file_location!
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
